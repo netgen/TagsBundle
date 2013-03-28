a0db3c2f6b58572c74e710d4df603f86715aa5b0
Edi Modrić <edi.modric@gmail.com>
2013-03-28 02:21:42 +0100

    Add simple representation of a single tag

--- Core/FieldType/Tags/Value.php
+++ Core/FieldType/Tags/Value.php
@@ -3,6 +3,7 @@
 namespace EzSystems\TagsBundle\Core\FieldType\Tags;
 
 use eZ\Publish\Core\FieldType\Value as BaseValue;
+use EzSystems\TagsBundle\Core\FieldType\Tags\Value\Tag;
 
 /**
  * Value for Tags field type
@@ -10,12 +11,37 @@ use eZ\Publish\Core\FieldType\Value as BaseValue;
 class Value extends BaseValue
 {
     /**
+     * @var \EzSystems\TagsBundle\Core\FieldType\Tags\Value\Tag[]
+     */
+    public $tags = array();
+
+    /**
+     * Constructor
+     *
+     * @param \EzSystems\TagsBundle\Core\FieldType\Tags\Value\Tag[] $tags
+     */
+    public function __construct( array $tags = array() )
+    {
+        // @TODO: Use TagCollection instead of plain array
+        $this->tags = $tags;
+    }
+
+    /**
      * Returns a string representation of the field value.
      *
      * @return string
      */
     public function __toString()
     {
-        // TODO: Implement __toString() method.
+        return implode(
+            ", ",
+            array_map(
+                function ( Tag $tag )
+                {
+                    return $tag->getKeyword();
+                },
+                $this->tags
+            )
+        );
     }
 }
