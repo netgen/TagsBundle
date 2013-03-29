037e2ebb02e4c38f05e6676bb34550734ec9a1d2
Edi Modrić <edi.modric@gmail.com>
2013-03-29 11:41:32 +0100

    Fix DependencyInjection extension

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace EzSystems\TagsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root( "ez_systems_tags" );
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
--- DependencyInjection/eZTagsExtension.php
+++ DependencyInjection/eZTagsExtension.php
@@ -10,15 +10,19 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 /**
  * This is the class that loads and manages the bundle configuration
  */
-class EzSystemsTagsExtension extends Extension
+class eZTagsExtension extends Extension
 {
     /**
      * {@inheritDoc}
      */
     public function load( array $configs, ContainerBuilder $container )
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration( $configuration, $configs );
+
         $loader = new YamlFileLoader( $container, new FileLocator( __DIR__ . "/../Resources/config" ) );
 
+        $loader->load( "default_settings.yml" );
         $loader->load( "fieldtypes.yml" );
         $loader->load( "storage_engines.yml" );
         $loader->load( "papi.yml" );
