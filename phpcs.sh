84449e4acff7ae88cdc3efba0042c538b79ded5e
Edi Modrić <edi.modric@gmail.com>
2013-03-31 23:03:10 +0200

    Add -s to phpcs call

--- phpcs.sh
+++ phpcs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-phpcs --standard=ezcs -p -v --extensions=php --encoding=utf-8 --ignore=_fixtures .
+phpcs --standard=ezcs -s -p -v --extensions=php --encoding=utf-8 --ignore=_fixtures .
