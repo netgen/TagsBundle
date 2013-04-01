95ed68bedb67ccf030847b5a9a0abd051d887720
Edi Modrić <edi.modric@gmail.com>
2013-04-01 10:36:11 +0200

    Add Tag value test

---
+++ Tests/Core/Repository/Values/Tags/TagTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EzSystems\TagsBundle\Tests\Core\Repository\Values\Tags;
+
+use EzSystems\TagsBundle\API\Repository\Values\Tags\Tag;
+use PHPUnit_Framework_TestCase;
+
+class TagTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \EzSystems\TagsBundle\API\Repository\Values\Tags\Tag::getProperties
+     */
+    public function testObjectProperties()
+    {
+        $object = new Tag();
+        $properties = $object->attributes();
+        $this->assertContains( "id", $properties, "Property not found" );
+        $this->assertContains( "parentTagId", $properties, "Property not found" );
+        $this->assertContains( "mainTagId", $properties, "Property not found" );
+        $this->assertContains( "keyword", $properties, "Property not found" );
+        $this->assertContains( "depth", $properties, "Property not found" );
+        $this->assertContains( "pathString", $properties, "Property not found" );
+        $this->assertContains( "modificationDate", $properties, "Property not found" );
+        $this->assertContains( "remoteId", $properties, "Property not found" );
+
+        // check for duplicates and double check existence of property
+        $propertiesHash = array();
+        foreach ( $properties as $property )
+        {
+            if ( isset( $propertiesHash[$property] ) )
+            {
+                $this->fail( "Property \"{$property}\" exists several times in properties list" );
+            }
+            else if ( !isset( $object->$property ) )
+            {
+                $this->fail( "Property \"{$property}\" does not exist on object, even though it was hinted to be there" );
+            }
+
+            $propertiesHash[$property] = 1;
+        }
+    }
+}
