4aa23a65d213d2ea568893c3d23a7291a4cfd938
Edi Modrić <edi.modric@gmail.com>
2013-04-03 00:50:06 +0200

    Update fixtures

--- Tests/_fixtures/content_objects.php
+++ Tests/_fixtures/content_objects.php
@@ -1,48 +1,48 @@
 <?php
 
 return array(
-    'ezcontentobject' => array(
+    "ezcontentobject" => array(
         array(
-            'contentclass_id' => '21',
-            'current_version' => '1',
-            'id' => '57',
-            'initial_language_id' => '8',
-            'language_mask' => '9',
-            'modified' => '1196268696',
-            'name' => 'Home',
-            'owner_id' => '14',
-            'published' => '1195480486',
-            'remote_id' => '8a9c9c761004866fb458d89910f52bee',
-            'section_id' => '1',
-            'status' => '1'
+            "contentclass_id" => "21",
+            "current_version" => "1",
+            "id" => "57",
+            "initial_language_id" => "8",
+            "language_mask" => "9",
+            "modified" => "1196268696",
+            "name" => "Home",
+            "owner_id" => "14",
+            "published" => "1195480486",
+            "remote_id" => "8a9c9c761004866fb458d89910f52bee",
+            "section_id" => "1",
+            "status" => "1"
         ),
         array(
-            'contentclass_id' => '20',
-            'current_version' => '1',
-            'id' => '58',
-            'initial_language_id' => '8',
-            'language_mask' => '8',
-            'modified' => '1332927282',
-            'name' => 'Contact Us',
-            'owner_id' => '14',
-            'published' => '1332927205',
-            'remote_id' => 'f8cc7a4cf8a964a1a0ea6666f5da7d0d',
-            'section_id' => '1',
-            'status' => '1'
+            "contentclass_id" => "20",
+            "current_version" => "1",
+            "id" => "58",
+            "initial_language_id" => "8",
+            "language_mask" => "8",
+            "modified" => "1332927282",
+            "name" => "Contact Us",
+            "owner_id" => "14",
+            "published" => "1332927205",
+            "remote_id" => "f8cc7a4cf8a964a1a0ea6666f5da7d0d",
+            "section_id" => "1",
+            "status" => "1"
         ),
         array(
-            'contentclass_id' => '3',
-            'current_version' => '1',
-            'id' => '59',
-            'initial_language_id' => '2',
-            'language_mask' => '3',
-            'modified' => '1343140541',
-            'name' => 'Partners',
-            'owner_id' => '14',
-            'published' => '1343140541',
-            'remote_id' => '14e4411b264a6194a33847843919451a',
-            'section_id' => '2',
-            'status' => '1'
+            "contentclass_id" => "3",
+            "current_version" => "1",
+            "id" => "59",
+            "initial_language_id" => "2",
+            "language_mask" => "3",
+            "modified" => "1343140541",
+            "name" => "Partners",
+            "owner_id" => "14",
+            "published" => "1343140541",
+            "remote_id" => "14e4411b264a6194a33847843919451a",
+            "section_id" => "2",
+            "status" => "1"
         )
     )
 );
