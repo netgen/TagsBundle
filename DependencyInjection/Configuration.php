d7d55496e4a4928f050264e4e8f2361ae0254c2e
Edi Modrić <edi.modric@gmail.com>
2013-04-03 11:40:29 +0200

    Update root configuration node name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root( "ez_systems_tags" );
+        $rootNode = $treeBuilder->root( "netgen_tags" );
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
