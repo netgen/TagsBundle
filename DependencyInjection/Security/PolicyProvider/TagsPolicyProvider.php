bf51e5b359da1c38d60f90bbc1827da20de61792
Edi Modrić <edi@netgen.hr>
2015-09-25 23:35:25 +0200

    Add a policy provider to expose tags module and its functions

---
+++ DependencyInjection/Security/PolicyProvider/TagsPolicyProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Netgen\TagsBundle\DependencyInjection\Security\PolicyProvider;
+
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Security\PolicyProvider\YamlPolicyProvider;
+
+class TagsPolicyProvider extends YamlPolicyProvider
+{
+    /**
+     * Returns an array of files where the policy configuration lies.
+     * Each file path MUST be absolute.
+     *
+     * @return array
+     */
+    public function getFiles()
+    {
+        return array(
+            __DIR__ . '/../../../Resources/config/policies.yml',
+        );
+    }
+}
--- NetgenTagsBundle.php
+++ NetgenTagsBundle.php
@@ -3,12 +3,36 @@
 namespace Netgen\TagsBundle;
 
 use eZ\Bundle\EzPublishLegacyBundle\LegacyBundles\LegacyBundleInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Netgen\TagsBundle\DependencyInjection\Security\PolicyProvider\TagsPolicyProvider;
 
 class NetgenTagsBundle extends Bundle implements LegacyBundleInterface
 {
+    /**
+     * Returns a list of legacy extension names.
+     *
+     * @return array List of legacy extension names to inject to ActiveExtensions
+     */
     public function getLegacyExtensionsNames()
     {
         return array('eztags');
+    }
+
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $eZExtension = $container->getExtension('ezpublish');
+        if (method_exists($eZExtension, 'addPolicyProvider')) {
+            $eZExtension->addPolicyProvider(new TagsPolicyProvider());
+        }
     }
 }
