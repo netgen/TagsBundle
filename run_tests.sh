8bcf3983b28c861b203ca803f630841ae5ece89a
Edi Modrić <edi@netgen.hr>
2016-03-04 10:05:26 +0100

    Use correct path to PHPUnit in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-phpunit -d memory_limit=-1 --colors -c phpunit.xml
-phpunit -d memory_limit=-1 --colors -c phpunit-integration-legacy.xml
+./vendor/bin/phpunit -d memory_limit=-1 --colors -c phpunit.xml
+./vendor/bin/phpunit -d memory_limit=-1 --colors -c phpunit-integration-legacy.xml
