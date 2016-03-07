fc0688649d22d71d131bfc65351a662e8967f5ee
Edi Modrić <edi@netgen.hr>
2016-03-07 10:16:43 +0100

    Max tags can go as low as 0

--- Core/FieldType/Tags/FormMapper.php
+++ Core/FieldType/Tags/FormMapper.php
@@ -12,34 +12,37 @@ class FormMapper implements FieldTypeFormMapperInterface
     {
         $fieldDefinitionForm
             ->add(
-                'subTreeLimit', 'integer', [
+                'subTreeLimit', 'integer', array(
                     'required' => false,
                     'property_path' => 'fieldSettings[subTreeLimit]',
                     'label' => 'field_definition.eztags.settings.subtree_limit',
                     'empty_data' => 0
-                ]
+                )
             )
             ->add(
-                'showDropDown', 'checkbox', [
+                'showDropDown', 'checkbox', array(
                     'required' => false,
                     'property_path' => 'fieldSettings[showDropDown]',
                     'label' => 'field_definition.eztags.settings.show_dropdown',
-                ]
+                )
             )
             ->add(
-                'hideRootTag', 'checkbox', [
+                'hideRootTag', 'checkbox', array(
                     'required' => false,
                     'property_path' => 'fieldSettings[hideRootTag]',
                     'label' => 'field_definition.eztags.settings.hide_root_tag',
-                ]
+                )
             )
             ->add(
-                'maxTags', 'integer', [
+                'maxTags', 'integer', array(
                     'required' => false,
                     'property_path' => 'fieldSettings[maxTags]',
                     'label' => 'field_definition.eztags.settings.max_tags',
-                    'empty_data' => 0
-                ]
+                    'empty_data' => 0,
+                    'attr' => array(
+                        'min' => 0
+                    )
+                )
             );
     }
 }
