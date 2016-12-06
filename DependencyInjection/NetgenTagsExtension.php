212068b0e8cdab34e99a2b0dc6d78bb60c4a3b84
Edi Modrić <edi@netgen.hr>
2016-12-06 12:20:46 +0100

    Remove obsolete Platform UI code

--- DependencyInjection/NetgenTagsExtension.php
+++ DependencyInjection/NetgenTagsExtension.php
@@ -64,7 +64,6 @@ class NetgenTagsExtension extends Extension implements PrependExtensionInterface
         $configs = array(
             'netgen_tags.yml' => 'netgen_tags',
             'ezpublish.yml' => 'ezpublish',
-            'ezplatform_ui.yml' => 'ez_platformui',
         );
 
         foreach ($configs as $fileName => $extensionName) {
