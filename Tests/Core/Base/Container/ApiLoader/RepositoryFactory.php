92b8334e29e4af528027595775c8eb80ceadfadd
Edi Modrić <edi@netgen.hr>
2016-12-06 10:51:58 +0100

    Fix tests

--- Tests/Core/Base/Container/ApiLoader/RepositoryFactory.php
+++ Tests/Core/Base/Container/ApiLoader/RepositoryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Netgen\TagsBundle\Tests\Core\Base\Container\ApiLoader;
 
+use eZ\Publish\Core\Base\Container\ApiLoader\FieldTypeNameableCollectionFactory;
 use eZ\Publish\Core\Base\Container\ApiLoader\RepositoryFactory as BaseRepositoryFactory;
 use eZ\Publish\Core\Base\Container\ApiLoader\FieldTypeCollectionFactory;
 use eZ\Publish\SPI\Persistence\Handler as PersistenceHandler;
@@ -14,10 +15,21 @@ class RepositoryFactory extends BaseRepositoryFactory
      */
     private $repositoryClass;
 
-    public function __construct($repositoryClass, FieldTypeCollectionFactory $fieldTypeCollectionFactory)
-    {
+    /**
+     * Collection of fieldTypes, lazy loaded via a closure.
+     *
+     * @var \eZ\Publish\Core\Base\Container\ApiLoader\FieldTypeNameableCollectionFactory
+     */
+    protected $fieldTypeNameableCollectionFactory;
+
+    public function __construct(
+        $repositoryClass,
+        FieldTypeCollectionFactory $fieldTypeCollectionFactory,
+        FieldTypeNameableCollectionFactory $fieldTypeNameableCollectionFactory
+    ) {
         $this->repositoryClass = $repositoryClass;
         $this->fieldTypeCollectionFactory = $fieldTypeCollectionFactory;
+        $this->fieldTypeNameableCollectionFactory = $fieldTypeNameableCollectionFactory;
     }
 
     /**
@@ -37,6 +49,7 @@ class RepositoryFactory extends BaseRepositoryFactory
             $searchHandler,
             array(
                 'fieldType' => $this->fieldTypeCollectionFactory->getFieldTypes(),
+                'nameableFieldTypes' => $this->fieldTypeNameableCollectionFactory->getNameableFieldTypes(),
                 'role' => array(
                     'policyMap' => array('tags' => array('add' => array('Tag' => true))),
                     'limitationTypes' => $this->roleLimitations,
