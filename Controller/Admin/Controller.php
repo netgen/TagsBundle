18ba27f4d3a09b312c18649cb591d34a4e61e5de
Edi Modrić <edi@netgen.hr>
2017-01-14 00:10:17 +0100

    Perform access check on tag admin controllers

--- Controller/Admin/Controller.php
+++ Controller/Admin/Controller.php
@@ -8,6 +8,18 @@ use Netgen\TagsBundle\API\Repository\Values\Tags\Tag;
 abstract class Controller extends BaseController
 {
     /**
+     * Ensures that only authenticated users can access to controller.
+     * It is not needed to call this method from actions
+     * as it's already called from base controller service.
+     *
+     * @see eztags.admin.controller.base service definition.
+     */
+    public function performAccessChecks()
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+    }
+
+    /**
      * Redirects to tag page or dashboard if tag is not provided.
      *
      * @param \Netgen\TagsBundle\API\Repository\Values\Tags\Tag $tag
