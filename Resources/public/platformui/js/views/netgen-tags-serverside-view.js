a94d28a237cc9717fa311939b04da9b6e1372227
Edi Modrić <edi@netgen.hr>
2017-01-15 15:06:06 +0100

    Override method for form serialization due to inability to handle checkboxes with the same name
    This version serializes the form as a string with key/value pairs

--- Resources/public/platformui/js/views/netgen-tags-serverside-view.js
+++ Resources/public/platformui/js/views/netgen-tags-serverside-view.js
@@ -11,5 +11,52 @@ YUI.add('netgen-tags-serverside-view', function (Y) {
                 }
             });
         },
+
+        _serializeForm: function (form, focusedNode) {
+            var data = [];
+
+            if ( this._isSubmitButton(focusedNode, form) ) {
+                data.push(focusedNode.getAttribute('name') + "=");
+            }
+
+            form.get('elements').each(function (field) {
+                var name = field.getAttribute('name'),
+                    type = field.get('type');
+
+                if ( !name ) {
+                    return;
+                }
+
+                /* jshint -W015 */
+                switch (type) {
+                    case 'button':
+                    case 'reset':
+                    case 'submit':
+                        break;
+                    case 'radio':
+                    case 'checkbox':
+                        if ( field.get('checked') ) {
+                            data.push(name + "=" + field.get('value'));
+                        }
+                        break;
+                    case 'select-multiple':
+                        if ( field.get('selectedIndex') >= 0 ) {
+                            field.get('options').each(function (opt) {
+                                if ( opt.get('selected') ) {
+                                    data.push(name + "=" + opt.get('value'));
+                                }
+                            });
+                        }
+                        break;
+                    default:
+                        // `.get('value')` returns the expected field value for
+                        // inputs, select-one and even textarea.
+                        data.push(name + "=" + field.get('value'));
+                }
+                /* jshint +W015 */
+            });
+
+            return data.join('&');
+        },
     });
 });
