53e9c66e74544adf1b75179202fda04d4d7ed9c5
Edi Modrić <edi@netgen.hr>
2017-01-24 13:13:54 +0100

    Add complete version info to Version class

--- Version.php
+++ Version.php
@@ -4,5 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
+    const VERSION = '3.0.0';
+    const VERSION_ID = 30000;
     const MAJOR_VERSION = 3;
+    const MINOR_VERSION = 0;
+    const RELEASE_VERSION = 0;
+    const EXTRA_VERSION = 'DEV';
 }
