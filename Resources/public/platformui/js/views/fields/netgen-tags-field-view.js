48f9b5ca78453ca2bb6c610b61d3f4fd45ab30b9
Edi Modrić <edi@netgen.hr>
2017-02-09 12:13:39 +0100

    Add isFieldEmpty method to tag field view for Platform UI

--- Resources/public/platformui/js/views/fields/netgen-tags-field-view.js
+++ Resources/public/platformui/js/views/fields/netgen-tags-field-view.js
@@ -3,7 +3,11 @@ YUI.add('netgen-tags-field-view', function (Y) {
 
     Y.namespace('Netgen.Tags.View.Field');
 
-    Y.Netgen.Tags.View.Field.TagsView = Y.Base.create('NetgenTagsFieldView', Y.eZ.FieldView, [], {});
+    Y.Netgen.Tags.View.Field.TagsView = Y.Base.create('NetgenTagsFieldView', Y.eZ.FieldView, [], {
+        _isFieldEmpty: function () {
+            return this._getFieldValue().length === 0;
+        },
+    });
 
     Y.eZ.FieldView.registerFieldView('eztags', Y.Netgen.Tags.View.Field.TagsView);
 });
