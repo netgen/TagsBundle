bcb1d61398a83331b5d0ce4003b2a9902fdf9a92
Edi Modrić <edi@netgen.hr>
2017-02-13 20:04:41 +0100

    Add handler that reinits tags tree after content type update

--- Resources/public/platformui/js/views/plugins/netgen-tags-content-type-edit-serverside-view-plugin.js
+++ Resources/public/platformui/js/views/plugins/netgen-tags-content-type-edit-serverside-view-plugin.js
@@ -6,6 +6,10 @@ YUI.add('netgen-tags-content-type-edit-serverside-view-plugin', function (Y) {
     Y.Netgen.Tags.Plugin.ContentTypeEditServerSideViewPlugin = Y.Base.create('NetgenTagsContentTypeEditServerSideViewPlugin', Y.Plugin.Base, [], {
         initializer: function () {
             Y.on('contentready', this.initTags, 'form[name="ezrepoforms_contenttype_update"]');
+
+            this.on('activeChange', function () {
+                this.after('htmlChange', this.initTags);
+            });
         },
 
         initTags: function () {
