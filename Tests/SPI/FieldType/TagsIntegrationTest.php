5188c8a9e7f371621eefab72f64ccc3fdd6135c6
Edi Modrić <edi@netgen.hr>
2017-02-13 14:04:06 +0100

    Do not use deprecated getMock method

--- Tests/SPI/FieldType/TagsIntegrationTest.php
+++ Tests/SPI/FieldType/TagsIntegrationTest.php
@@ -129,7 +129,7 @@ class TagsIntegrationTest extends BaseIntegrationTest
                         new LanguageHandlerMock()
                     ),
                 ),
-                $this->getMock(TagsService::class)
+                $this->createMock(TagsService::class)
             )
         );
     }
