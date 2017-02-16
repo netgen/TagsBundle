affa8b2d1a510bc1785d1c101f42cfb07dde662e
Edi Modrić <edi@netgen.hr>
2017-02-16 14:32:49 +0100

    Handle all responses with PJAX-Location header

--- Resources/public/platformui/js/views/services/netgen-tags-serverside-view-service.js
+++ Resources/public/platformui/js/views/services/netgen-tags-serverside-view-service.js
@@ -3,8 +3,18 @@ YUI.add('netgen-tags-serverside-view-service', function (Y) {
 
     Y.namespace('Netgen.Tags.Service');
 
+    var PJAX_DONE_REDIRECT = 205,
+        PJAX_LOCATION_HEADER = 'PJAX-Location';
+
     Y.Netgen.Tags.Service.ServerSideViewService = Y.Base.create('NetgenTagsServerSideViewService', Y.eZ.ServerSideViewService, [], {
         _parseResponse: function (response) {
+            var app = this.get('app'),
+                pjaxLocation = response.getResponseHeader(PJAX_LOCATION_HEADER);
+
+            if ( response.status === PJAX_DONE_REDIRECT && pjaxLocation ) {
+                app.navigate(this._getAdminRouteUri(pjaxLocation));
+            }
+
             Y.eZ.ServerSideViewService.prototype._parseResponse.call(this, response);
 
             var head = Y.one('head');
