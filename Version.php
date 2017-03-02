bbf8c0b087ef66fd6f02a6b6a8b3506d3db42b3f
Edi Modrić <edi@netgen.hr>
2017-03-02 13:44:16 +0100

    3.0.0 RC1 release

--- Version.php
+++ Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.0.0-DEV';
+    const VERSION = '3.0.0-RC1';
     const VERSION_ID = 30000;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 0;
     const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = 'RC1';
 }
