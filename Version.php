2794b219372ef3e374e996446894bc12f639b904
Edi Modrić <edi.modric@gmail.com>
2017-10-16 16:55:33 +0200

    Update version number

--- Version.php
+++ Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.0.7';
-    const VERSION_ID = 30007;
+    const VERSION = '3.0.8';
+    const VERSION_ID = 30008;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 0;
-    const RELEASE_VERSION = 7;
+    const RELEASE_VERSION = 8;
     const EXTRA_VERSION = '';
 }
