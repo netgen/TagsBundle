86d38f3236c5234ab31086eef06fd575bb2df56b
Edi Modrić <edi.modric@gmail.com>
2017-11-21 17:13:07 +0100

    Fix out of memory exceptions on eZ Platform v2

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.1.3';
-    const VERSION_ID = 30103;
+    const VERSION = '3.1.4';
+    const VERSION_ID = 30104;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 1;
-    const RELEASE_VERSION = 3;
+    const RELEASE_VERSION = 4;
     const EXTRA_VERSION = '';
 }
