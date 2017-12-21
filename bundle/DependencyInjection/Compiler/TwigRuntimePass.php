da3cf8a90d4022d0fde570cf5cf27509a54ce762
Edi Modrić <edi.modric@gmail.com>
2017-12-21 12:03:02 +0100

    Do not run TwigRuntimePass if Twig is not available

--- bundle/DependencyInjection/Compiler/TwigRuntimePass.php
+++ bundle/DependencyInjection/Compiler/TwigRuntimePass.php
@@ -10,6 +10,10 @@ class TwigRuntimePass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
+        if (!$container->has('twig')) {
+            return;
+        }
+
         if ($container->has('twig.runtime_loader')) {
             // If official Twig runtime loader exists,
             // we skip using our custom runtime loader
