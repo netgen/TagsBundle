a6717eefeaa10f00297e3ad9ae36fa66d6b96901
Edi Modrić <edi.modric@gmail.com>
2017-12-22 10:15:22 +0100

    Bump version to 3.2.0 final

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.2.0-DEV';
+    const VERSION = '3.2.0';
     const VERSION_ID = 30200;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 2;
     const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 }
