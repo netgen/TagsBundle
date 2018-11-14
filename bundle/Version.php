cdc3057c11529274df62f54ba98409621c968851
Edi Modrić <edi.modric@gmail.com>
2018-11-14 13:15:12 +0100

    Bump version to 3.3.4

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.3.3';
-    const VERSION_ID = 30303;
+    const VERSION = '3.3.4';
+    const VERSION_ID = 30304;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 3;
-    const RELEASE_VERSION = 3;
+    const RELEASE_VERSION = 4;
     const EXTRA_VERSION = '';
 }
