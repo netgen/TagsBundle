fa45fd1c425e228aeeb62b4fa7a8b7d103121118
Edi Modrić <edi.modric@gmail.com>
2019-04-03 12:54:24 +0200

    Update version to 3.4.0 final

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.4.0-DEV';
+    const VERSION = '3.4.0';
     const VERSION_ID = 30400;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 4;
     const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 }
