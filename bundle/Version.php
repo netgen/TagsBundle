8b42eaaa6084f32990ec479d53e59dc24a8c9521
Edi Modrić <edi.modric@gmail.com>
2019-06-24 13:24:39 +0200

    Bump version to 3.4.2

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.4.1';
-    const VERSION_ID = 30401;
+    const VERSION = '3.4.2';
+    const VERSION_ID = 30402;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 1;
+    const RELEASE_VERSION = 2;
     const EXTRA_VERSION = '';
 }
