8f8d78811cd1570d75b874c7e9ee51a5ab572ceb
Edi Modrić <edi.modric@gmail.com>
2019-12-19 16:32:22 +0100

    Update version

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.4.5';
-    const VERSION_ID = 30405;
+    const VERSION = '3.4.6';
+    const VERSION_ID = 30406;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 5;
+    const RELEASE_VERSION = 6;
     const EXTRA_VERSION = '';
 }
