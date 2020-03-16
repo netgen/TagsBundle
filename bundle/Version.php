12e0e32a40cd1ec91f5cc7f74a14a0ab1ae587cb
Edi Modrić <edi.modric@gmail.com>
2020-03-16 14:28:17 +0100

    Fix version

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.4.7';
-    const VERSION_ID = 30407;
+    const VERSION = '3.4.6';
+    const VERSION_ID = 30406;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 7;
+    const RELEASE_VERSION = 6;
     const EXTRA_VERSION = '';
 }
