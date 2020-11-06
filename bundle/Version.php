9b2b444f7e3091aea5e40f2b031f9e2d37b115b0
Edi Modrić <edi.modric@gmail.com>
2020-11-06 09:31:20 +0100

    Bump version to 3.4.8

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\TagsBundle;
 
 abstract class Version
 {
-    const VERSION = '3.4.7';
-    const VERSION_ID = 30407;
+    const VERSION = '3.4.8';
+    const VERSION_ID = 30408;
     const MAJOR_VERSION = 3;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 7;
+    const RELEASE_VERSION = 8;
     const EXTRA_VERSION = '';
 }
