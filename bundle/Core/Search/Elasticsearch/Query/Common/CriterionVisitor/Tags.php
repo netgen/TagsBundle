0f4859d56d0a97927d6d3270afac192f1560f1de
Petar Jakopec <petar.jakopec0@gmail.com>
2023-07-25 14:53:07 +0200

    FINAWEB-53 add visitors

---
+++ bundle/Core/Search/Elasticsearch/Query/Common/CriterionVisitor/Tags.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\TagsBundle\Core\Search\Elasticsearch\Query\Common\CriterionVisitor;
+
+use Ibexa\Contracts\Core\Persistence\Content\Type\Handler;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Elasticsearch\Query\CriterionVisitor;
+use Ibexa\Core\Search\Common\FieldNameResolver;
+
+use function array_merge;
+
+abstract class Tags implements CriterionVisitor
+{
+    private FieldNameResolver $fieldNameResolver;
+
+    /**
+     * For tag-queries which aren't field-specific.
+     */
+    private Handler $contentTypeHandler;
+
+    /**
+     * Identifier of the field type that criterion can handle.
+     */
+    private string $fieldTypeIdentifier;
+
+    /**
+     * Name of the field type's indexed field that criterion can handle.
+     */
+    private string $fieldName;
+
+    /**
+     * @param FieldNameResolver $fieldNameResolver
+     * @param Handler $contentTypeHandler
+     * @param string $fieldTypeIdentifier
+     * @param string $fieldName
+     */
+    public function __construct(FieldNameResolver $fieldNameResolver, Handler $contentTypeHandler, string $fieldTypeIdentifier, string $fieldName)
+    {
+        $this->fieldNameResolver = $fieldNameResolver;
+        $this->contentTypeHandler = $contentTypeHandler;
+        $this->fieldTypeIdentifier = $fieldTypeIdentifier;
+        $this->fieldName = $fieldName;
+    }
+
+    protected function getSearchFields(Criterion $criterion): array
+    {
+        if ($criterion->target !== null) {
+            return $this->fieldNameResolver->getFieldTypes(
+                $criterion,
+                $criterion->target,
+                $this->fieldTypeIdentifier,
+                $this->fieldName,
+            );
+        }
+
+        $targetFieldTypes = [];
+        foreach ($this->contentTypeHandler->getSearchableFieldMap() as $fieldDefinitions) {
+            foreach ($fieldDefinitions as $fieldIdentifier => $fieldDefinition) {
+                if (!isset($fieldDefinition['field_type_identifier'])) {
+                    continue;
+                }
+
+                if ($fieldDefinition['field_type_identifier'] !== $this->fieldTypeIdentifier) {
+                    continue;
+                }
+
+                $fieldTypes = $this->fieldNameResolver->getFieldTypes(
+                    $criterion,
+                    $fieldIdentifier,
+                    $this->fieldTypeIdentifier,
+                    $this->fieldName,
+                );
+
+                $targetFieldTypes[] = $fieldTypes;
+            }
+        }
+
+        return array_merge(...$targetFieldTypes);
+    }
+}
---
+++ bundle/Core/Search/Elasticsearch/Query/Common/CriterionVisitor/Tags/TagKeyword.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\TagsBundle\Core\Search\Elasticsearch\Query\Common\CriterionVisitor\Tags;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\Operator;
+use Ibexa\Contracts\Elasticsearch\Query\CriterionVisitor;
+use Ibexa\Contracts\Elasticsearch\Query\LanguageFilter;
+use Ibexa\Core\Base\Exceptions\InvalidArgumentException;
+use Ibexa\Elasticsearch\ElasticSearch\QueryDSL\BoolQuery;
+use Ibexa\Elasticsearch\ElasticSearch\QueryDSL\TermQuery;
+use Netgen\TagsBundle\API\Repository\Values\Content\Query\Criterion\TagKeyword as APITagKeyword;
+use Netgen\TagsBundle\Core\Search\Elasticsearch\Query\Common\CriterionVisitor\Tags;
+use Netgen\TagsBundle\Core\Search\Elasticsearch\QueryDSL\PrefixQuery;
+
+use function count;
+
+final class TagKeyword extends Tags
+{
+    public function supports(Criterion $criterion, LanguageFilter $languageFilter): bool
+    {
+        return $criterion instanceof APITagKeyword;
+    }
+
+    public function visit(CriterionVisitor $dispatcher, Criterion $criterion, LanguageFilter $languageFilter): array
+    {
+        $criterion->value = (array) $criterion->value;
+        $searchFields = $this->getSearchFields($criterion);
+        $isLikeOperator = $criterion->operator === Operator::LIKE;
+
+        if (count($searchFields) === 0) {
+            throw new InvalidArgumentException(
+                '$criterion->target',
+                "No searchable fields found for the given criterion target '{$criterion->target}'.",
+            );
+        }
+
+        $query = new BoolQuery();
+        foreach ($searchFields as $name => $fieldType) {
+            foreach ($criterion->value as $value) {
+                if ($isLikeOperator) {
+                    $query->addShould(new PrefixQuery($name, $value));
+                } else {
+                    $query->addShould(new TermQuery($name, $value));
+                }
+            }
+        }
+
+        return $query->toArray();
+    }
+}
