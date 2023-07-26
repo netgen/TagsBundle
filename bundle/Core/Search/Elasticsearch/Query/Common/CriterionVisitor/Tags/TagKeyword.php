ebb0e601d1130be04887c2be180fb22c3e424fde
Petar Jakopec <petar.jakopec0@gmail.com>
2023-07-26 12:00:38 +0200

    FINAWEB-53 hint criterion value as string for phpstan

--- bundle/Core/Search/Elasticsearch/Query/Common/CriterionVisitor/Tags/TagKeyword.php
+++ bundle/Core/Search/Elasticsearch/Query/Common/CriterionVisitor/Tags/TagKeyword.php
@@ -39,6 +39,9 @@ final class TagKeyword extends Tags
 
         $query = new BoolQuery();
         foreach ($searchFields as $name => $fieldType) {
+            /**
+             * @var string $value
+             */
             foreach ($criterion->value as $value) {
                 if ($isLikeOperator) {
                     $query->addShould(new PrefixQuery($name, $value));
