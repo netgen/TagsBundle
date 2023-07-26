f4b8cce7ea0a8525641b58a5d8cd5841bd2a9ac7
Petar Jakopec <petar.jakopec0@gmail.com>
2023-07-26 10:33:55 +0200

    FINAWEB-53 use constructor property promotion

--- bundle/Core/Search/Elasticsearch/Query/Common/CriterionVisitor/Tags.php
+++ bundle/Core/Search/Elasticsearch/Query/Common/CriterionVisitor/Tags.php
@@ -13,29 +13,12 @@ use function array_merge;
 
 abstract class Tags implements CriterionVisitor
 {
-    private FieldNameResolver $fieldNameResolver;
-
-    /**
-     * For tag-queries which aren't field-specific.
-     */
-    private Handler $contentTypeHandler;
-
-    /**
-     * Identifier of the field type that criterion can handle.
-     */
-    private string $fieldTypeIdentifier;
-
-    /**
-     * Name of the field type's indexed field that criterion can handle.
-     */
-    private string $fieldName;
-
-    public function __construct(FieldNameResolver $fieldNameResolver, Handler $contentTypeHandler, string $fieldTypeIdentifier, string $fieldName)
-    {
-        $this->fieldNameResolver = $fieldNameResolver;
-        $this->contentTypeHandler = $contentTypeHandler;
-        $this->fieldTypeIdentifier = $fieldTypeIdentifier;
-        $this->fieldName = $fieldName;
+    public function __construct(
+        private readonly FieldNameResolver $fieldNameResolver,
+        private readonly Handler $contentTypeHandler,
+        private readonly string $fieldTypeIdentifier,
+        private readonly string $fieldName,
+    ) {
     }
 
     protected function getSearchFields(Criterion $criterion): array
--- bundle/Core/Search/Elasticsearch/QueryDSL/PrefixQuery.php
+++ bundle/Core/Search/Elasticsearch/QueryDSL/PrefixQuery.php
@@ -8,14 +8,10 @@ use Ibexa\Elasticsearch\ElasticSearch\QueryDSL\Query;
 
 final class PrefixQuery implements Query
 {
-    private ?string $field;
-
-    private ?string $value;
-
-    public function __construct(?string $field = null, ?string $value = null)
-    {
-        $this->field = $field;
-        $this->value = $value;
+    public function __construct(
+        private ?string $field = null,
+        private ?string $value = null,
+    ) {
     }
 
     public function withField(string $field): self
