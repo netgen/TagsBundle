ebb46fe23eb0e80544bbb193b5e574d91bea0a7a
pkamps <pkamps@mugo.ca>
2023-11-13 16:01:58 +0100

    New Criterion "TagSubtree" matching the "path_string" of tags

---
+++ bundle/API/Repository/Values/Content/Query/Criterion/TagSubtree.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\TagsBundle\API\Repository\Values\Content\Query\Criterion;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\Operator;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\Operator\Specifications;
+
+/**
+ * A criterion that matches content based on tag subtree that is located in one of the fields.
+ *
+ * Supported operators:
+ * - EQ: matches against one tag ID
+ */
+final class TagSubtree extends Criterion
+{
+    /**
+     * @param int|int[] $value One or more tag IDs that must be matched
+     * @param string|null $target Field definition identifier to use
+     */
+    public function __construct($value, ?string $target = null)
+    {
+        parent::__construct($target, null, $value);
+    }
+
+    public function getSpecifications(): array
+    {
+        return [
+            new Specifications(
+                Operator::EQ,
+                Specifications::FORMAT_SINGLE,
+                Specifications::TYPE_INTEGER | Specifications::TYPE_STRING
+            ),
+        ];
+    }
+}
