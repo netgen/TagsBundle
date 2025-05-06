f4fcd8fc9af768df34a6a997d3f04be0dba3a031
Petar Španja <petar@spanja.info>
2025-05-06 15:04:17 +0200

    NGSTACK-971: add merged tag to the MergeTagsEvent

--- bundle/API/Repository/Events/Tags/MergeTagsEvent.php
+++ bundle/API/Repository/Events/Tags/MergeTagsEvent.php
@@ -9,7 +9,12 @@ use Netgen\TagsBundle\API\Repository\Values\Tags\Tag;
 
 final class MergeTagsEvent extends AfterEvent
 {
-    public function __construct(private Tag $targetTag) {}
+    public function __construct(private Tag $tag, private Tag $targetTag) {}
+
+    public function getTag(): Tag
+    {
+        return $this->tag;
+    }
 
     public function getTargetTag(): Tag
     {
